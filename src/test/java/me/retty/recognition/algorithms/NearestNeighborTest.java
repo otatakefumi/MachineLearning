1c05787edd0d4d61a0414652755aeca04e1bfa50
TakefumiOta <takefumi@retty.me>
2015-09-22 19:04:24 +0900

    nn: add test file

---
+++ src/test/java/me/retty/recognition/algorithms/NearestNeighborTest.java
@@ -0,0 +1,26 @@
+package me.retty.recognition.algorithms;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by takefumiota on 2015/09/22.
+ */
+public class NearestNeighborTest {
+
+    @Test
+    public void testStartLearning() throws Exception {
+
+    }
+
+    @Test
+    public void testExec() throws Exception {
+
+    }
+
+    @Test
+    public void testGetPrototypes() throws Exception {
+
+    }
+}
