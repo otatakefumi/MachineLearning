75f3a90631926138071b4499771666786b9f8c5b
TakefumiOta <takefumi@retty.me>
2015-09-22 17:12:48 +0900

    add base class

---
+++ src/me/retty/recognition/base/Dimension.java
@@ -0,0 +1,72 @@
+package me.retty.recognition.base;
+
+/**
+ * Created by takefumiota on 2015/09/22.
+ * Indicate dimension vector that used for pattern recognition(input, weight, and so on)
+ */
+public class Dimension<T> {
+    /**
+     * width and height of this dimension
+     */
+    protected int width, height;
+    /**
+     * actual data for this dimension
+     */
+    protected Object[][] data;
+
+    /**
+     * constructor
+     * @param width width of this dimension
+     * @param height height of this dimension
+     */
+    public Dimension(int width, int height) {
+        if (width <= 0) {
+            width = 1;
+        }
+        if (height <= 0) {
+            height = 1;
+        }
+        this.width = width;
+        this.height = height;
+        this.data = new Object[height][width];
+    }
+
+    /**
+     * get value of this dimension
+     * @param x x-axis of value
+     * @param y y-axis of value
+     * @return value of this dimension(type: T)
+     */
+    @SuppressWarnings("unchecked")
+    public T getValue(int x, int y) {
+        return (T)this.data[y][x];
+    }
+
+    /**
+     * set value on this dimension
+     * @param x x-axis you wanna set value
+     * @param y y-axis you wanna set value
+     * @param value value you wanna set
+     * @return this dimension after set value(type: Dimension&lt;T&gt;)
+     */
+    public Dimension<T> setData(int x, int y, T value) {
+        data[y][x] = value;
+        return this;
+    }
+
+    /**
+     * get the width of this dimension
+     * @return width of this dimension(type: int)
+     */
+    public int getWidth() {
+        return this.width;
+    }
+
+    /**
+     * get the height of this dimension
+     * @return width of this dimension(type: int)
+     */
+    public int getHeight() {
+        return this.height;
+    }
+}
---
+++ src/me/retty/recognition/base/Result.java
@@ -0,0 +1,8 @@
+package me.retty.recognition.base;
+
+/**
+ * Created by takefumiota on 2015/09/22.
+ * Indicate result of pattern recognition
+ */
+public class Result {
+}
