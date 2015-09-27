b0d36bcceb1aa0afdead4139c2097c522e855b46
TakefumiOta <takefumi@retty.me>
2015-09-27 16:39:57 +0900

    modify-main: modify class of main variable

--- src/main/java/me/retty/recognition/Main.java
+++ src/main/java/me/retty/recognition/Main.java
@@ -3,19 +3,18 @@ package me.retty.recognition;
 import me.retty.recognition.algorithms.ExtendedNearestNeighbor;
 import me.retty.recognition.algorithms.NearestNeighbor;
 import me.retty.recognition.base.Config;
+import me.retty.recognition.base.modules.algorithm.IAlgorithm;
 
 public class Main {
     public static String DATA_BASE_DIR = "/Users/takefumiota/work/machine_learning/data/";
     public static String LEARN_DATA_DIR = "learn_gray";
     public static String TEST_DATA_DIR = "test_gray";
-//    public static int WIDTH = 300;
-//    public static int HEIGHT = 300;
 
     public static void main(String[] args) {
         //NearestNeighbor nn = new NearestNeighbor(null);
-        ExtendedNearestNeighbor nn = new ExtendedNearestNeighbor(new Config().set("prototype_length", 250));
+        IAlgorithm algorithm = new ExtendedNearestNeighbor(new Config().set("prototype_length", 250));
 
-        nn.learn(new Config().set("inputDirPath", DATA_BASE_DIR + LEARN_DATA_DIR));
-        nn.verify(new Config().set("inputDirPath", DATA_BASE_DIR + TEST_DATA_DIR));
+        algorithm.learn(new Config().set("inputDirPath", DATA_BASE_DIR + LEARN_DATA_DIR));
+        algorithm.verify(new Config().set("inputDirPath", DATA_BASE_DIR + TEST_DATA_DIR));
     }
 }
